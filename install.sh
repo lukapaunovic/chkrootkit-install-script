ecbc2add4505a2edbb2dfaee864de3f0c797f603
zyedzah <162665818+zyedzah@users.noreply.github.com>
2024-03-08 22:30:57 -0500

    nonrhelbased.sh
    Replaced yum commands with apt commands for package management.
    
    Removed the unnecessary line with the Chkrootkit download URL (as the script already downloads and extracts it).
    
    Used sudo for system-level commands.

--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 # Installing prerequisites
+apt update
+apt -y install build-essential
 
-yum -y group install "Development Tools"
-yum -y install glibc-static libstdc++-static
+# Download and install chkrootkit
+chkrootkit_version="0.58b"
+chkrootkit_url="https://fossies.org/linux/misc/chkrootkit-${chkrootkit_version}.tar.gz"
 
-# Install
+mkdir -p /tmp/chkrootkit_install
+cd /tmp/chkrootkit_install || exit 1
 
-https://fossies.org/linux/misc/chkrootkit-0.58b.tar.gz
-tar xvf chkrootkit*.tar.gz
-rm -rf chkrootkit*.tar.gz
-cd chkrootkit-*
+curl -LO "$chkrootkit_url"
+tar xvf "chkrootkit-${chkrootkit_version}.tar.gz"
+rm -f "chkrootkit-${chkrootkit_version}.tar.gz"
+
+cd "chkrootkit-${chkrootkit_version}" || exit 1
+
+# Compile and install chkrootkit
 make sense
-mv -f $(pwd) /usr/local
-mv /usr/local/chkrootkit-* /usr/local/chkrootkit
-ln -s /usr/local/chkrootkit/chkrootkit /usr/local/bin/chkrootkit
+sudo mv -f "$(pwd)" /usr/local
+sudo mv "/usr/local/chkrootkit-${chkrootkit_version}" /usr/local/chkrootkit
+sudo ln -s "/usr/local/chkrootkit/chkrootkit" /usr/local/bin/chkrootkit
+
+# Run chkrootkit and filter output
 /usr/local/bin/chkrootkit | grep -v -E "not(hing)? (infected|found|tested)"
+
+# Cleanup temporary directory
+cd / && sudo rm -rf /tmp/chkrootkit_install
+
+#End
